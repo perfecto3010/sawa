884f0cd39ec79944a57031b55b2ff3ae6c711bc3
perfecto3010 <perfecto3010@gmail.com>
2026-03-21 11:56:42 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'sawa-v1';
+const assets = [
+  './',
+  './index.html',
+  './gamesData.js',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/833/833472.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
